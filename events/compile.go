5d16b1d80c90fffbb20111e2bc913f42e55d31c0
lemonit_cn <liuri@lemonit.cn>
2018-12-18 17:27:33 +0800

    wj

---
+++ events/compile.go
@@ -0,0 +1,72 @@
+package events
+
+import (
+	"io/ioutil"
+	"lemon-robot-cli/logger"
+	"lemon-robot-cli/utils/io"
+	"os"
+	"strings"
+)
+
+var groovyScriptDest = ""
+
+func CompileGroovy() {
+	//遍历当前工程的src/main/groovy文件夹，把所有groovy文件拼接
+	logger.Info("Prepare to build groovy scripts")
+	loc, _ := os.Getwd()
+	if !isStandardPath(loc) {
+		logger.Error("The current directory is not a standard Lemon Robot Task project!")
+		os.Exit(1)
+	}
+	// reset
+	groovyScriptDest = ""
+	logger.Info("Prepare to read your scripts")
+	readGroovyScripts(loc)
+	logger.Info("Prepare to write dest script file")
+	writeGroovyScriptDest(loc+"/src/main/resources/__lemon_robot_task.groovy", groovyScriptDest)
+}
+
+/**
+判断是否为标准的LemonRobot任务目录
+ */
+func isStandardPath(path string) bool {
+	return io.PathExists(path + "/pom.xml")
+}
+
+func readGroovyScripts(groovyPath string) {
+	rd, _ := ioutil.ReadDir(groovyPath)
+	for _, fi := range rd {
+		filePath := groovyPath + "/" + fi.Name()
+		if fi.IsDir() {
+			readGroovyScripts(filePath)
+		} else {
+			if strings.HasSuffix(fi.Name(), ".groovy") {
+				logger.Info("Find groovy script file: " + filePath)
+				file, _ := os.Open(filePath)
+				scriptContent, _ := ioutil.ReadAll(file)
+				groovyScriptDest += string(scriptContent) + "\n\n"
+			}
+		}
+	}
+}
+
+func writeGroovyScriptDest(destPath string, script string) {
+	if io.PathExists(destPath) {
+		errRemove := os.Remove(destPath)
+		if errRemove != nil {
+			logger.Error("Can not remove exists dest file: " + destPath)
+			os.Exit(1)
+		}
+	}
+	_, errCreate := os.Create(destPath)
+	if errCreate != nil {
+		logger.Error("Can not create the groovy dest file: " + destPath)
+		os.Exit(2)
+	}
+	errWrite := ioutil.WriteFile(destPath, []byte(script), 0600)
+	if errWrite != nil {
+		logger.Error("Can not write the groovy dest file: " + destPath)
+		os.Exit(3)
+	}
+	logger.Info("Write dest file complete! : " + destPath)
+}
--- main.go
+++ main.go
@@ -1,17 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"lemon-robot-cli/events"
 	"lemon-robot-cli/logger"
 	"os"
 )
 
+var eventsMapping = map[string]func(){
+	"build":          events.Build,
+	"compile.groovy": events.CompileGroovy}
+
 func main() {
+	printLogo()
 	if len(os.Args) <= 1 {
 		logger.Error("Please tell me what you want to do, for example:")
 		logger.Error("lr build")
 		logger.Error("lr run")
 		os.Exit(-1)
 	}
-	events.Build()
+	eventName := os.Args[1]
+	_, containEvent := eventsMapping[eventName]
+	if containEvent {
+		loc, _ := os.Getwd()
+		logger.Info("Current path: " + loc)
+		logger.Info("Invoke event: " + os.Args[1])
+		eventsMapping[os.Args[1]]()
+	} else {
+		logger.Error("The event not support: " + eventName)
+	}
+
+}
+
+func printLogo() {
+	fmt.Println(`
+ _                               ______       _                
+| |                             (_____ \     | |           _   
+| |      _____ ____   ___  ____  _____) )___ | |__   ___ _| |_ 
+| |     | ___ |    \ / _ \|  _ \|  __  // _ \|  _ \ / _ (_   _)
+| |_____| ____| | | | |_| | | | | |  \ \ |_| | |_) ) |_| || |_ 
+|_______)_____)_|_|_|\___/|_| |_|_|   |_\___/|____/ \___/  \__)
+
+	`)
+	fmt.Printf("🍋 %c[0;0;32mLemon Robot Cli - Version: 1.0.0 - https://www.lemonit.cn%c[0m 🍋\n\n", 0x1B, 0x1B)
 }
