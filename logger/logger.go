e4d00b40aad109c5dc99069d83b685b2065330de
lemonit_cn <liuri@lemonit.cn>
2018-12-19 09:28:16 +0800

    change windows logger

--- logger/logger.go
+++ logger/logger.go
@@ -3,6 +3,8 @@ package logger
 import (
 	"fmt"
 	"lemon-robot-cli/utils/date"
+	"runtime"
+	"strings"
 )
 
 func Debug(msg string) {
@@ -25,5 +27,9 @@ var logTypeList = [] string{"DEBG", "WARN", "ERRO", "INFO"}
 var logColorList = [] int{0, 33, 31, 34}
 
 func log(msg string, logType int) {
-	fmt.Printf("%c[0;0;%dm[%s %s] %s%c[0m\n", 0x1B, logColorList[logType], logTypeList[logType], date.GetCurrentTimeFormatedStr(), msg, 0x1B)
+	if strings.Contains(runtime.GOOS, "windows") {
+		fmt.Printf("[%s %s] %s", logTypeList[logType], date.GetCurrentTimeFormatedStr(), msg)
+	} else {
+		fmt.Printf("%c[0;0;%dm[%s %s] %s%c[0m\n", 0x1B, logColorList[logType], logTypeList[logType], date.GetCurrentTimeFormatedStr(), msg, 0x1B)
+	}
 }
