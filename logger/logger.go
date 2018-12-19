2270f2c582c1e38fb2bd05442f5e5a16f4fd622b
lemonit_cn <liuri@lemonit.cn>
2018-12-19 09:30:25 +0800

    change windows logger

--- logger/logger.go
+++ logger/logger.go
@@ -28,7 +28,7 @@ var logColorList = [] int{0, 33, 31, 34}
 
 func log(msg string, logType int) {
 	if strings.Contains(runtime.GOOS, "windows") {
-		fmt.Printf("[%s %s] %s", logTypeList[logType], date.GetCurrentTimeFormatedStr(), msg)
+		fmt.Printf("[%s %s] %s\n", logTypeList[logType], date.GetCurrentTimeFormatedStr(), msg)
 	} else {
 		fmt.Printf("%c[0;0;%dm[%s %s] %s%c[0m\n", 0x1B, logColorList[logType], logTypeList[logType], date.GetCurrentTimeFormatedStr(), msg, 0x1B)
 	}
